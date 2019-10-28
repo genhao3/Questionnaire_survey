1d6ff6426513e0bc61c33365afd4d878a7cd92be
genhao3 <1148695207@qq.com>
2019-10-28 08:52:43 +0800

    最终
    最终

--- src/component/questionTable/questionTable.vue
+++ src/component/questionTable/questionTable.vue
@@ -1,19 +1,34 @@
 <template>
-  <el-table :data="paperData" border style="width: 100%">
-    <el-table-column prop="id" label="ID" width="180"></el-table-column>
-    <el-table-column prop="name" label="问卷" width="180"></el-table-column>
-    <el-table-column prop="status" label="状态" width="100">
-      <template slot-scope="scope">
-        <el-tag :type="renderStatusTag(scope.row.status)" disable-transitions>{{scope.row.status | changeStatusTag }}</el-tag>
-      </template>
-    </el-table-column>
-    <tableBtn :btnType="tableType"  ></tableBtn>
-  </el-table>
+  <div style="position: relative">
+    <el-table :data="paperData" border style="width: 100%">
+      <el-table-column prop="id" label="ID" width="180"></el-table-column>
+      <el-table-column prop="name" label="问卷" width="180"></el-table-column>
+      <el-table-column prop="status" label="状态" width="100">
+        <template slot-scope="scope" >
+          <el-tag :type="renderStatusTag(scope.row.status)" disable-transitions>{{scope.row.status | changeStatusTag }}</el-tag>
+        </template>
+      </el-table-column>
+      <tableBtn :btnType="tableType"  @getQRcode="bindQRCode"></tableBtn>
+    </el-table>
+    <el-dialog
+            title="生成的二维码"
+            :visible.sync="dialogVisible"
+            width="30%"
+            class="elDialog"
+            >
+      <div id="qrCode" ref="qrCodeDiv" class="QRcode"></div>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false" class="QRcodeBtn">确 定</el-button>
+       </span>
+    </el-dialog>
+
+  </div>
 </template>
 
 <script>
 import ajaxAllPaperObj from "../../api/questionPaper";
 import tableBtn from "../../component/questionTable/tableBtn";
+import QRCode from 'qrcodejs2';
 export default {
   name: "questionTable",
   components: {
@@ -25,6 +40,7 @@ export default {
   },
   data() {
     return {
+        dialogVisible:false
     };
   },
   methods: {
@@ -45,7 +61,21 @@ export default {
       }
       return result;
     },
-
+    bindQRCode (data) {
+        this.dialogVisible = true
+        this.$nextTick(() => {
+        this.$refs.qrCodeDiv.innerHTML = ''
+        var url = 'http://182.61.26.241:18080/dist/'+data.href
+        new QRCode(this.$refs.qrCodeDiv, {
+            text: url,
+            width: 200,
+            height: 200,
+            colorDark: "#333333", //二维码颜色
+            colorLight: "#ffffff", //二维码背景色
+            correctLevel: QRCode.CorrectLevel.L//容错率，L/M/H
+        })
+    })
+    }
   },
   filters: {
    changeStatusTag: function(val) {
@@ -66,9 +96,23 @@ export default {
       return result;
     }
   },
-
 };
 </script>
 
 <style scoped lang="scss">
+  .QRcode{
+    position: relative;
+    left: 50%;
+    margin-left: -100px;
+  }
+  .QRcodeBtn{
+    position: relative;
+    left:-50%;
+    transform: translateX(50%);
+  }
+  @media screen and (max-width: 760px) {
+    el-dialog__body{
+      width: 100%;
+    }
+  }
 </style>
