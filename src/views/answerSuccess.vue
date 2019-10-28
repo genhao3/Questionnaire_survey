ba3f1d60b2d65d00f6e4d645049886a1652a2f04
genhao3 <1148695207@qq.com>
2019-10-28 08:55:34 +0800

    最终
    最终

--- src/views/answerSuccess.vue
+++ src/views/answerSuccess.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <img src="../assets/404_cloud.png" alt="问卷完成">
-        <p>感谢完成问卷填写</p>
+    <div class="successContainer">
+        <div class="imgContainer">
+            <img src="../assets/404_cloud.png" class="imgLeft" alt="问卷完成">
+            <img src="../assets/404_cloud.png" class="imgMid" alt="问卷完成">
+            <img src="../assets/404_cloud.png" class="imgRight" alt="问卷完成">
+            <div class="textSuccess">感谢完成问卷填写</div>
+        </div>
+        <div class="textContainer">
+
+        </div>
     </div>
 </template>
 
@@ -12,5 +19,59 @@
 </script>
 
 <style scoped>
-
+.successContainer{
+    position: absolute;
+    margin: auto;
+    top: 50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    font-size: 22px;
+    color:#5cb87a;
+    overflow: hidden;
+    display: flex;
+}
+.imgContainer{
+    position: relative;
+    width: 350px;
+    height: 350px;
+}
+.textContainer{
+}
+.imgLeft{
+    width: 80px;
+    position: absolute;
+    top: 0px;
+    left: 60px;
+}
+.imgRight{
+    width: 62px;
+    position: absolute;
+    top: 10px;
+    left: 220px;
+}
+.imgMid{
+    width: 46px;
+    position: absolute;
+    top: 100px;
+    left: 150px;
+}
+.textSuccess{
+    opacity: 0;
+    text-align: center;
+    margin-bottom: 20px;
+    margin-top: 50px;
+    animation-name:slideUp;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+}
+@keyframes slideUp {
+    0%{
+        transform: translateY(60px);
+        opacity: 0;
+    }
+    100%{
+        transform: translateY(0px);
+        opacity: 1;
+    }
+}
 </style>
